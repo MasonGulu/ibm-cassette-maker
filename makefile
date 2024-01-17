23515ce6f042c4dbb1460da5de806b168c280009
Mason Gulu <mason.gulu@gmail.com>
2024-01-16 21:42:47 -0500

    Command line argument refactoring

--- makefile
+++ makefile
@@ -4,13 +4,13 @@ WOBJS=bulid/main.wo
 all : ibmwriter.out ibmwriter.exe
 
 ibmwriter.out : build/main.o
-	g++ -o ibmwriter.out -static build/main.o
+	g++ -o build/ibmwriter.out -static build/main.o
 
 build/main.o : src/main.cpp
 	g++ -o build/main.o -c src/main.cpp
 
 ibmwriter.exe : build/main.wo
-	x86_64-w64-mingw32-g++ -o ibmwriter.exe -static build/main.wo
+	x86_64-w64-mingw32-g++ -o build/ibmwriter.exe -static build/main.wo
 
 build/main.wo : src/main.cpp
 	x86_64-w64-mingw32-g++ -o build/main.wo -c src/main.cpp
--- src/main.cpp
+++ src/main.cpp
@@ -25,23 +25,30 @@
 
 using namespace std;
 
-#define version "1.2"
+#define version "1.3"
+#define LSB(value) (value & 0x00FF)
+#define MSB(value) (value >> 16)
 
-
-std::queue<double> audioData;
+std::queue<int8_t> audioData;
 uint16_t CRC = 0xFFFF;
 
 const float SECONDS_ONE  = 0.001f;   // Time in seconds
 const float SECONDS_TWO = 0.0005f; // Time in seconds
-const float FRAMERATE = 4000.f;
+float FRAMERATE = 4000.f;
 
-const float FRAMES_ONE  = (SECONDS_ONE / 2) * FRAMERATE;  // Frames per cycle, two cycles per number
-const float FRAMES_ZERO = (SECONDS_TWO / 2) * FRAMERATE; // Frames per cycle, two cycles per number
+float FRAMES_ONE  = (SECONDS_ONE / 2) * FRAMERATE;  // Frames per cycle, two cycles per number
+float FRAMES_ZERO = (SECONDS_TWO / 2) * FRAMERATE; // Frames per cycle, two cycles per number
 
-const double VALUE_ON = 0xFFFF;
-const double VALUE_OFF = -0xFFFF;
+const int8_t VALUE_ON = 127;
+const int8_t VALUE_OFF = -128;
 
 const uint16_t MAX_INPUT_FILE_SIZE = 0xFFFF;
+
+void set_framerate(float framerate) {
+    FRAMERATE = framerate;
+    FRAMES_ONE  = (SECONDS_ONE / 2) * FRAMERATE;
+    FRAMES_ZERO = (SECONDS_TWO / 2) * FRAMERATE;
+}
 
 void _writeCRC(bool bit) {
     bool carry = 0;
@@ -63,209 +70,237 @@ void _writeBit(bool valueBit) {
     }
 }
 
-void writeByte(uint8_t value) {
+void write_byte(uint8_t value) {
     for (int x = 7; x > -1; x--) {
         _writeBit((value>>x)&1);
     }
 }
 
-void generateLeader() {
-    for (int x = 0; x < 256; x++) writeByte(0xFF);
+void generate_leader() {
+    for (int x = 0; x < 256; x++) write_byte(0xFF);
     // ^ 256 bytes of FF
     _writeBit(0);
     // ^ Sync bit
-    writeByte(0x16);
+    write_byte(0x16);
 }
 
-void generateTrailer() {
-    writeByte(0xFF);
-    writeByte(0xFF);
-    writeByte(0xFF);
-    writeByte(0xFF);
+void generate_trailer() {
+    write_byte(0xFF);
+    write_byte(0xFF);
+    write_byte(0xFF);
+    write_byte(0xFF);
 }
 
-void generateSilence(int seconds=1) {
+void generate_silence(int seconds=1) {
     for (int x = 0; x < FRAMERATE*seconds; x++) {
         // 1 second of silence at beginning of file
         audioData.push(0);
     }
 }
 
-uint8_t MSB(uint16_t value) {
-    return ((value & 0xFF00) >> 8);
-}
-
-uint8_t LSB(uint16_t value) {
-    return (value & 0x00FF);
-}
-
-void writeCRC() {
+void write_CRC() {
     uint16_t CRCValue = CRC ^ 0xFFFF;
-    writeByte(MSB(CRCValue));
-    writeByte(LSB(CRCValue));
+    write_byte(MSB(CRCValue));
+    write_byte(LSB(CRCValue));
 }
 
-void generateBasicHeader(char* filename, uint16_t segment, uint16_t offset, int size, uint8_t headertype) {
+void generate_basic_header(const char* basic_filename, uint16_t segment, uint16_t offset, int size, uint8_t headertype) {
     // Basic Header removal
-    generateLeader();
+    generate_leader();
     CRC = 0xFFFF;
-    writeByte(0xA5);
+    write_byte(0xA5);
     // ^ Marks this block as BASIC
     bool endOfName = false;
     for (int x = 0; x < 8; x++) {
-        if (!endOfName && filename[x] == 0) endOfName = true;
-        if (endOfName) writeByte(0b00100000);
-        else writeByte(filename[x]);
+        if (!endOfName && basic_filename[x] == 0) endOfName = true;
+        if (endOfName) write_byte(0b00100000);
+        else write_byte(basic_filename[x]);
     }
     // ^ Filename
-    writeByte(headertype);
+    write_byte(headertype);
     // ^ Memory area
-    writeByte(LSB(size));
-    writeByte(MSB(size));
+    write_byte(LSB(size));
+    write_byte(MSB(size));
     // ^ File size
-    writeByte(LSB(segment));
-    writeByte(MSB(segment));
+    write_byte(LSB(segment));
+    write_byte(MSB(segment));
     // ^ Segment
-    writeByte(LSB(offset));
-    writeByte(MSB(offset));
+    write_byte(LSB(offset));
+    write_byte(MSB(offset));
     // ^ Offset
 
-    writeByte(0x00);
+    write_byte(0x00);
     for (int x = 0; x < 239; x++) {
-        writeByte(0x01);
+        write_byte(0x01);
     }
     // ^ Filler
-    writeCRC();
+    write_CRC();
 
-    generateTrailer();
-    generateSilence();
+    generate_trailer();
+    generate_silence();
 }
 
-void writeBlock(char* inputfile, int size, int &base, int &written, int bytes, bool &fileend, bool replaceEndline=false) {
+void write_block(char* inputfile, int size, int &base, int &written, int bytes, bool &fileend, bool replaceEndline=false) {
     for (int x = 0; x < bytes; x++) {
         if (base+x+1 > size) fileend = true;
-        if (fileend) writeByte(0b00100000); // Fill rest of block with NULL
+        if (fileend) write_byte(0b00100000); // Fill rest of block with NULL
         else {
-            if (replaceEndline) (inputfile[base+x] == 0x0A) ? writeByte(0x0D) : writeByte(inputfile[base+x]);
-            else writeByte(inputfile[base+x]);
+            if (replaceEndline) (inputfile[base+x] == 0x0A) ? write_byte(0x0D) : write_byte(inputfile[base+x]);
+            else write_byte(inputfile[base+x]);
             written++;
         }
     }
-    writeCRC();
+    write_CRC();
     base+=bytes;
 }
 
-void binWrite(char* inputfile, int size, char* filename, uint16_t segment, uint16_t offset, bool basicHeader = true) {
-    generateSilence();
+void bin_write(char* inputfile, int size, const char* basic_filename, uint16_t segment, uint16_t offset, bool basicHeader = true) {
+    generate_silence();
     if (basicHeader) {
-        generateBasicHeader(filename, segment, offset, size, 0x01);
+        generate_basic_header(basic_filename, segment, offset, size, 0x01);
     }
     bool fileend = false;
     int base = 0;
     int written = 0;
-    generateLeader();
+    generate_leader();
     while (!fileend) {
         CRC = 0xFFFF;
-        writeBlock(inputfile, size, base, written, 256, fileend);
+        write_block(inputfile, size, base, written, 256, fileend);
     }
     cout << "Processed " << written << " bytes." << endl;
-    generateTrailer();
+    generate_trailer();
     // Leadout
-    generateSilence();
+    generate_silence();
 }
 
 
-void imgWrite(char* inputfile, int size, char* filename, uint16_t segment, uint16_t offset) {
-    generateSilence(3);
+void img_write(char* inputfile, int size, const char* basic_filename, uint16_t segment, uint16_t offset, bool _) {
+    generate_silence(3);
     bool fileend = false;
     int base = 0;
     int written = 0;
     int tracksWritten = 0;
     while (!fileend) {
-        generateLeader();
+        generate_leader();
         for (int i = 0; i < 32; i++) {
             CRC = 0xFFFF;
-            writeBlock(inputfile, size, base, written, 256, fileend);
+            write_block(inputfile, size, base, written, 256, fileend);
         }
         tracksWritten++;
         cout << "Wrote " << tracksWritten << " tracks; " << written << " bytes." << endl;
         if (tracksWritten == 40) fileend=true;
         // For some reason my fileend logic doesn't work in this specific instance, so hardcode it! Filesize is already enforced anyways.
-        generateTrailer();
-        generateSilence(10);
+        generate_trailer();
+        generate_silence(10);
     }
 }
 
-void asciiWrite(char* inputfile, int size, char* filename, uint16_t segment, uint16_t offset) {
-    generateSilence();
-    generateBasicHeader(filename, segment, offset, size, 0b01000000);
+void ascii_write(char* inputfile, int size, const char* basic_filename, uint16_t segment, uint16_t offset, bool _) {
+    generate_silence();
+    generate_basic_header(basic_filename, segment, offset, size, 0b01000000);
 
     bool fileend = false;
     int base = 0;
     int written = 0;
     while (!fileend) {
-        generateLeader();
+        generate_leader();
         CRC = 0xFFFF;
-        ((size - written) > 255) ? writeByte(0x00) : writeByte(size-written);
-        writeBlock(inputfile, size, base, written, 255, fileend, true);
-        generateTrailer();
-        generateSilence();
+        ((size - written) > 255) ? write_byte(0x00) : write_byte(size-written);
+        write_block(inputfile, size, base, written, 255, fileend, true);
+        generate_trailer();
+        generate_silence();
     }
     cout << "Processed " << written << " bytes." << endl;
 }
 
-int main(int argCount, char *argValues[]) {
-    cout << "This program is licensed under GPLv2 and comes with ABSOLUTELY NO WARRANTY." << endl;
-    cout << "Version " << version << endl;
-    if (argCount < 6) {
-        cout << "Usage: " << argValues[0] << " <raw, bin, bas, img(*)> [input] [output] [segment] [offset]" << endl;
-        cout << "(*) img is for 320kb disk images, see README." << endl;
-        return 0;
+typedef void (*write_func_t) (char*,int,const char*,uint16_t,uint16_t,bool);
+
+// Parsed arguments
+char* if_buffer;
+int if_size;
+write_func_t process_func;
+bool do_header;
+string basic_filename;
+string filename;
+uint16_t segment = 0;
+uint16_t offset = 0;
+
+
+int parse_flags(int argc, char* argv[]) {
+    for (int i = 4; i < argc; i++) {
+        if (strncmp(argv[i], "-86box", 6) == 0) {
+            set_framerate(44100.0f);
+        } else if (strncmp(argv[i], "-segment=", 9) == 0) {
+            segment = atoi(argv[i+9]);
+        } else if (strncmp(argv[i], "-offset=", 8) == 0) {
+            offset = atoi(argv[i+8]);
+        } else {
+            printf("Unrecognized argument %s.\n", argv[i]);
+        }
+    }
+}
+
+int parse_args(int argc, char* argv[]) {
+    if (argc < 4) {
+        printf("Usage: %s <raw, bin, bas, img(*)> input basic_name [flags]\n", argv[0]);
+        printf("(*) img is for 320kb disk images, see README.\n");
+        printf("basic_name up to 8 character BASIC filename.\n\n");
+        printf("Flags:\n");
+        printf("-segment=<value>\n-offset=<value>\n");
+        printf("-86box Change bitrate to 44.1kHz for 86box compat.\n");
+        return 1;
     }
 
-    streampos ifSize;
-    char* ifBuffer;
-    ifstream ifStream (argValues[2], ios::binary|ios::ate);
+    parse_flags(argc, argv);
+
+    basic_filename = string(argv[3]).substr(0, 8);
+    filename = basic_filename.append(".wav");
+
+    ifstream ifStream (argv[2], ios::binary|ios::ate);
     if (ifStream.is_open()) {
-        ifSize = ifStream.tellg();
-        ifBuffer = new char [ifSize];
+        if_size = ifStream.tellg();
+        if_buffer = new char [if_size];
         ifStream.seekg(0, ios::beg);
-        ifStream.read(ifBuffer, ifSize);
+        ifStream.read(if_buffer, if_size);
         ifStream.close();
     }
     else {
         cout << "ERROR: The input file cannot be opened." << endl;
-        return 0;
+        return 1;
     }
-    if ((ifSize > MAX_INPUT_FILE_SIZE) && (argValues[1][2] != 'g')) {
+    if ((if_size > MAX_INPUT_FILE_SIZE) && (argv[1][2] != 'g')) {
         cout << "ERROR: The input file is larger than " << MAX_INPUT_FILE_SIZE << " bytes." << endl;
-        return 0;
+        return 1;
     }
-    // At this point the entire file contents is loaded into ifBuffer
-    unsigned sampleCount = 1;
-    
-    switch(argValues[1][2]) {
+    do_header = true;
+    switch(argv[1][2]) {
         case 'n': // bin
-            binWrite(ifBuffer, (int)ifSize, argValues[3], atoi(argValues[4]), atoi(argValues[5]));
+            process_func = bin_write;
             break;
         case 's': // bas
-            asciiWrite(ifBuffer, (int)ifSize, argValues[3], atoi(argValues[4]), atoi(argValues[5]));
+            process_func = ascii_write;
             break;
         case 'w': // raw
-            binWrite(ifBuffer, (int)ifSize, argValues[3], atoi(argValues[4]), atoi(argValues[5]), false);
+            process_func = bin_write;
+            do_header = false;
             break;
         case 'g': // img
-            if (ifSize != 327680) {
+            if (if_size != 327680) {
                 cout << "File is not 320kb." << endl;
-                return 0;
+                return 1;
             }
-            // buffer is already interlaced.
-            imgWrite(ifBuffer, (int)ifSize, argValues[3], atoi(argValues[4]), atoi(argValues[5]));
+            process_func = ascii_write;
             break;
         default:
             cout << "Invalid argument." << endl;
-            return 0;
+            return 1;
     }
+    return 0;
+}
+
+void process_data() {
+    process_func(if_buffer, if_size, basic_filename.c_str(), segment, offset, do_header);
+
     AudioFile<double> audiofile;
     AudioFile<double>::AudioBuffer buffer;
     int length = audioData.size();
@@ -273,6 +308,7 @@ int main(int argCount, char *argValues[]) {
     buffer[0].resize(length);
     audiofile.setAudioBufferSize(1, length);
     audiofile.setSampleRate(FRAMERATE);
+    audiofile.setBitDepth(8);
     int index = 0;
     
     while (audioData.size() > 0) {
@@ -282,5 +318,17 @@ int main(int argCount, char *argValues[]) {
     }
     cout << "Processed " << index << " frames." << endl;
     audiofile.setAudioBuffer(buffer);
-    audiofile.save(argValues[3], AudioFileFormat::Wave);
+    audiofile.save(filename, AudioFileFormat::Wave);
+}
+
+int main(int argc, char *argv[]) {
+    cout << "This program is licensed under GPLv2 and comes with ABSOLUTELY NO WARRANTY." << endl;
+    cout << "Version " << version << endl;
+
+    int arg_result = parse_args(argc, argv);
+    if (arg_result != 0) {
+        return arg_result;
+    }
+
+    process_data();
 }
