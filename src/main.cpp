bb7e405d271078753b71870916113e108adb20fb
Mason Gulu <mason.gulu@gmail.com>
2024-01-17 21:14:38 -0500

    I never actually changed the license in the code?

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,3 @@
-/*
- This will take a binary or ascii file and convert it into a recording
- which can then be loaded on an IBM 5150
- Copyright (C) 2021 Mason Gulu
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License along
-   with this program; if not, write to the Free Software Foundation, Inc.,
-   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
- If you have any questions or comments you may contact me at mason.gulu@gmail.com
-*/
 #include <iostream>
 #include <fstream>
 #include <queue>
@@ -322,7 +302,7 @@ void process_data() {
 }
 
 int main(int argc, char *argv[]) {
-    cout << "This program is licensed under GPLv2 and comes with ABSOLUTELY NO WARRANTY." << endl;
+    cout << "This program is licensed under MIT and comes with ABSOLUTELY NO WARRANTY." << endl;
     cout << "Version " << version << endl;
 
     int arg_result = parse_args(argc, argv);
