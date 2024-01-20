4078ecf91db8811061d5a71bfdbf94f9c6a57aa7
Mason Gulu <mason.gulu@gmail.com>
2024-01-20 13:44:25 -0500

    broken WIP of C formatting

---
+++ floppysrc/tools.asm
@@ -0,0 +1,107 @@
+    .model    small
+    .code
+
+    extern    test_:near
+    extern    _disk_base:near
+    global    entry:near
+    global    print_:near
+    global    read_:near
+    global    format_:near
+    global    reset_:near
+    global    verify_:near
+entry:
+
+    push    ds
+    push    es
+
+    mov    ax, cs
+    mov    ds, ax
+    mov    es, ax
+
+    mov    bx, ss
+    mov    old_ss, bx
+    mov    old_sp, sp
+
+    mov    ss, ax
+    mov    sp, stack_top
+
+    call    test_
+
+    mov    ax, old_ss
+    mov    ss, ax
+    mov    sp, old_sp
+
+    pop    es
+    pop    ds
+
+    retf
+
+print_ proc near
+push si
+mov si, ax
+SUB_print:
+    ; Print a 0-terminated ASCII string in DS:SI
+	mov ax,0E00h
+	lodsb				; byte[DS:SI] => AL
+	cmp al,00			; Terminating character
+	je _SUB_print_printfinished
+	push bp				; I had an ancient machine that destroyed these two!!!
+	push si
+	int 10h				; Blurp it out
+	pop si
+	pop bp
+	jmp SUB_print
+_SUB_print_printfinished:
+    pop si
+	ret
+print_ endp
+
+read_ proc near
+    mov ax, 0
+    int 16h
+    ret
+read_ endp
+
+format_ proc near
+    ; char sectors, format_loc loc, format_desc *info, char track
+    mov ah, 5
+    mov ch, cl
+    mov cl, 0
+    int 13h
+    mov al, ah ; return char
+    ret
+format_ endp
+
+; AX, DX, BX, CX register parameter order, for some reason????
+verify_ proc near
+    ; char sectors, format_loc loc, format_desc *info, char track
+    mov ah, 4
+    mov ch, cl ; track #
+    mov cl, 1 ; start at sector 1
+    ; es:bx is pointer to memory buffer, not really sure what this is used for
+    int 13h
+    adc al, 0 ; return success char
+    ret
+verify_ endp
+
+reset_ proc near
+    push dx
+    mov dx, ax
+    mov ax, 0
+    int 13h
+    pop dx
+    ret
+reset_ endp
+
+    .data?
+
+old_ss    dw    ?
+old_sp    dw    ?
+
+    stack segment 'stack'
+    
+    db    200 dup (?)
+stack_top:
+    stack ends
+
+    end
---
+++ floppysrc/tools.c
@@ -0,0 +1,228 @@
+
+#define _FP_OFF(__p) ((unsigned)(__p))
+#define _FP_SEG(__p) ((unsigned)((unsigned long)(void __far*)(__p) >> 16))
+
+extern void print(const char*);
+#define SCANCODE(value) (value >> 8)
+#define ASCIICODE(value) (value & 0xFF)
+extern int read(); // call int16h; scan code, ascii code
+extern char format(char sectors, struct format_loc loc, struct format_desc *info, char track);
+extern void reset(char drive);
+extern char verify(char sectors, struct format_loc loc, struct format_desc *info, char track);
+
+void print_int(unsigned int i) {
+    char to_print[6];
+    int index = 0;
+    to_print[5] = 0;
+    for (index = 4; index >= 0; index--) {
+        to_print[index] = (i % 10) + 48;
+        i /= 10;
+        if (i == 0) {
+            break;
+        }
+    }
+    print(to_print+index);
+}
+struct format_desc {
+    char track;
+    char head;
+    char sector; // Start at 1 - 9
+    char bps; // Bytes per sector, 2 = 512bits
+};
+
+struct format_desc format_table[9] = {
+    {0,0,1,2},
+    {0,0,2,2},
+    {0,0,3,2},
+    {0,0,4,2},
+    {0,0,5,2},
+    {0,0,6,2},
+    {0,0,7,2},
+    {0,0,8,2},
+    {0,0,9,2},
+};
+
+struct format_loc {
+    char drive;
+    char head;
+};
+
+struct disk_base {
+    char spec1;
+    char spec2;
+    char motor_wait;
+    char N; // bytes per sector (2=512bps)
+    char EOT; // last track on the disk (8 default)
+    char gap_length; // 0x2A default
+    char DTL; // 0xFF default
+    char fmt_gap_length; // gap length for format 0x50 default
+    char fill_byte; // fill byte for format 0xF6 default
+    char settle_time; // head settle time (25ms default)
+    char start_time; // motor start time (4 /8ths s default)
+} disk_base;
+
+void cpy(char __far * from, char __far * to, int n) {
+    int i = 0;
+    for (; i < n; i++) {
+        to[i] = from[i];
+    }
+}
+
+char newline[] = "\r\n";
+
+int __far * INT_1E = 0x0000 :> 0x0078; // int 1E
+
+struct disk_base __far *OLD_BASE;
+
+char* get_error_str(int code) {
+    switch (code) {
+        case 1:
+            return "Invalid Command";
+        case 2:
+            return "Cannot Find Address Mark";
+        case 3:
+            return "Attempted Write on Write Protected Disk";
+        case 4:
+            return "Sector Not Found";
+        case 5:
+            return "Reset Failed";
+        case 6:
+            return "Disk Change Line Active";
+        case 7:
+            return "Drive Parameter Activity Failed";
+        case 8:
+            return "DMA Overrun";
+        case 9:
+            return "Attempt To DMA Over 64kb Boundary";
+        case 0xA:
+            return "Bad Sector Detected";
+        case 0xB:
+            return "Bad Track Detected";
+        case 0xC:
+            return "Media Type Not Found";
+        case 0xD:
+            return "Invalid Number Of Sectors";
+        case 0xE:
+            return "Control Data Address Mark Detected";
+        case 0xF:
+            return "DMA Out Of Range";
+        case 0x10:
+            return "CRC/ECC Data Error";
+        case 0x11:
+            return "ECC Corrected Data Error";
+        case 0x20:
+            return "Controller Failure";
+        case 0x40:
+            return "Seek Failure";
+        case 0x80:
+            return "Drive Timed Out, Assumed Not Ready";
+        case 0xAA:
+            return "Drive Not Ready";
+        case 0xCC:
+            return "Write Error";
+        case 0xE0:
+            return "Status Error";
+        case 0xFF:
+            return "Sense Operation Failed";
+        default:
+            return "Unknown Error";
+    }
+}
+
+void update_disk_base(char tracks) {
+    int segment = 0x2000;
+    int offset = (int) &disk_base;
+    struct disk_base __far * LOCAL_BASE = segment :> offset;
+    OLD_BASE = (struct disk_base __far *) INT_1E[0] :> INT_1E[1];
+    cpy((char __far*)OLD_BASE, (char __far*)LOCAL_BASE, sizeof(struct disk_base));
+
+    // disk_base.EOT = tracks;
+    // if (tracks == 9) {
+    //     disk_base.gap_length = 0x18; // https://github.com/andreas-jonsson/imagedisk/blob/main/IMD.C#L241C18-L241C19
+    // }
+
+    INT_1E[0] = _FP_SEG(LOCAL_BASE);
+    INT_1E[1] = _FP_OFF(LOCAL_BASE);
+}
+
+void revert_disk_base() {
+    INT_1E[0] = _FP_SEG(OLD_BASE);
+    INT_1E[1] = _FP_OFF(OLD_BASE);
+}
+
+void update_format_table(char track, char head) {
+    int i;
+    struct format_desc *desc;
+    for (i = 0; i < 9; i++) {
+        desc = format_table+i;
+        desc->track = track;
+        desc->head = head;
+    }
+}
+
+int do_format(char sectors) {
+    char track;
+    char head;
+    int errors;
+    int failed_tracks = 0;
+    struct format_loc loc = {0, 0};
+    for (track = 0; track < 3; track++) {
+        for (head = 0; head < 2; head++) {
+            errors = 0;
+            print("Formatting Track: ");
+            print_int(track);
+            print(" Head: ");
+            print_int(head);
+            print("\r");
+            loc.head = head;
+            update_format_table(track, head);
+            while (1) {
+                char status = format(sectors, loc, format_table, track);
+                if (status == 96 || status == 0) break;
+                print("\r\nFormat errored: ");
+                print_int(status);
+                print(" ");
+                print(get_error_str(status));
+                print(newline);
+                reset(0);
+                errors++;
+                if (errors == 3) {
+                    print("Failed to format track.\r\n");
+                    return 1;
+                }
+            }
+            // format succeeded, now verify
+            errors = verify(sectors, loc, format_table, track);
+            if (errors == 1) {
+                print("\r\nFailed to verify track. It is likely bad.\r\n");
+                failed_tracks++;
+            }
+        }
+    }
+    if (failed_tracks == 0) {
+        print("Disk was successfully formatted.\r\n");
+    } else {
+        print_int(failed_tracks);
+        print(" tracks failed to format.\r\n");
+        print("Disk cannot be for image writing.\r\n");
+        return 1;
+    }
+    return 0;
+}
+void test() {
+    char input;
+    int sectors;
+    print("Push 8/9 to format\r\n");
+    input = ASCIICODE(read());
+    if (input == '8') {
+        sectors = 8;
+    } else if (input == '9') {
+        sectors = 9;
+    } else {
+        print("Cancelled\r\n");
+        return;
+    }
+    update_disk_base(sectors);
+    do_format(sectors);
+    revert_disk_base();
+}
